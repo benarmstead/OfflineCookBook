883bd5358ede96877b4dd85752aebce556f3a366
Ben Armstead <contact@benarmstead.co.uk>
2022-01-11 23:20:58 +0000

    Fix error

--- lib/screens/all_recipes.dart
+++ lib/screens/all_recipes.dart
@@ -3,11 +3,11 @@ import 'package:offline_cook_book/custom_widgets/recipe_list.dart';
 import 'package:offline_cook_book/objects/recipe.dart';
 
 class AllRecipesScreen extends StatelessWidget {
-  AllRecipesScreen(this.allRecipes, {Key? key}) : super(key: key);
+  const AllRecipesScreen(this.allRecipes, {Key? key}) : super(key: key);
 
-  List<Recipe> allRecipes;
+  final List<Recipe> allRecipes;
 
-  double width = 400;
+  final double width = 400;
 
   @override
   Widget build(context) {
